0738fb5879d9e8784c69799bdceb9f205587cae5
= <=>
2022-10-24 21:49:22 -0400

    Initial commit. It can say, 'Hello,' and that's it.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+class Leaderboard extends React.Component {
+  render() {
+    return (
+      <div>Hello.</div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Leaderboard />)
